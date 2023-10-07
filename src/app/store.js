05b3f7b505db366c256df668f999f668b8647e78
weiwang0305 <weiwang0305@gmail.com>
2023-10-07 13:38:46 -0400

    reducer maybe working

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import stateReducer from '../features/profileState/profileStateSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    updateState: stateReducer,
+  },
 });
