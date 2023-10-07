fda171ed8a5cead3b917169c4c0abec0f6c9522b
Jdave1125 <Jdave1125@gmail.com>
2023-10-07 10:35:13 -0400

    BE initial commit Co-authored-by: HadriChan <HadriChan@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+
+
 const App = () => {
   return <h1>Dating App</h1>;
 };
+
+ReactDOM.render(<App />, document.getElementById('root'))
 export default App;
