83000d3a126d03e4b1c376edbf968b786034e793
weiwang0305 <weiwang0305@gmail.com>
2023-10-07 12:04:29 -0400

    Setup initial Redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 const App = () => {
   return <h1>Dating App</h1>;
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import App from './App.jsx';
 import PrefPage from './preference.jsx';
 
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 
-render(<PrefPage />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <PrefPage />
+  </Provider>,
+  document.getElementById('root')
+);
