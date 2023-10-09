2cdfd73b1edad352b825854959c669c9b9211178
HadriChan <124481646+HadriChan@users.noreply.github.com>
2023-10-09 10:16:41 -0400

    worked on css

--- src/preference.jsx
+++ src/preference.jsx
@@ -4,27 +4,44 @@ const PrefPage = () => {
   return (
     <div className="pref-container">
       <div className="title">Findr</div>
-      <div className="quote"> Whatchu want in your potential shawty?</div>
+      <div className="quote">
+        <div>Whatchu want in your potential shawty? </div>
+        Choose up to three interests:
+      </div>
       <form
         className="submit-form"
         action="http://localhost:3000/search"
         method="GET"
       >
         <label for="preference1">
-          Preference 1:
-          <input name="preference1" class="prefbox" id="pref-box-1" />
+          <input
+            name="preference1"
+            class="prefbox"
+            placeholder="Your first interest..."
+            id="pref-box-1"
+          />
         </label>
         <label for="preference2">
-          Preference 2:
-          <input name="preference2" class="prefbox" id="pref-box-2" />
+          <input
+            name="preference2"
+            class="prefbox"
+            placeholder="Your second interest..."
+            id="pref-box-2"
+          />
         </label>
         <label for="preference3">
-          Preference 3:
-          <input name="preference3" class="prefbox" id="pref-box-3" />
+          <input
+            name="preference3"
+            class="prefbox"
+            placeholder="Your third interest..."
+            id="pref-box-3"
+          />
         </label>
-        <button className="primary" type="submit">
-          Match Me!
-        </button>
+        <div style={{ whiteSpace: "pre" }}>
+          <button className="primary" type="submit">
+            Match Me!
+          </button>
+        </div>
       </form>
     </div>
   );
