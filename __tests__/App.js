3979ad976f73fb7830861a63a981711c6baa5723
david tezza <dtezza12@gmail.com>
2023-10-10 20:03:36 -0400

    implmenting jest tests, not able to import jest-dom library currently

---
+++ __tests__/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import App from '../src/App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+
+describe('testing React Components', () => {
+    
+    beforeEach(() => {
+        render(<BrowserRouter><App /></BrowserRouter>)
+    })
+
+    test('renders the Prefpage component for the "/" route', () => {
+      const buttons = screen.getAllByRole('button');
+      // console.log(buttons);
+      expect(buttons.length).toEqual(2);
+      console.log("buttons: ", buttons)
+      expect(buttons[0]).toHaveClassName('primary');
+      expect(buttons[1]).toHaveClassName('secondary');
+      expect(screen.getByText('Findr')).toHaveClass('title');
+    })
+})
